87aa02179f29208095467aaf42142c60692f54a1
Paulo Menezes <paulo.hgmenezes@gmail.com>
2021-03-08 02:00:18 -0300

    load bootstrap component html and typescript

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
 import fs from 'fs';
 import path from 'path';
-import { ArrayLiteralExpression, Node, Project, SourceFile, Statement } from 'ts-morph';
+import { ArrayLiteralExpression, Block, Node, Project, PropertyDeclaration, SourceFile } from 'ts-morph';
 import ts from 'typescript';
-import { AST } from './ast';
+import {
+  AST,
+  ASTFile,
+  Component,
+  ComponentConstructorParameter,
+  ComponentElement,
+  ComponentElementProperty,
+  ComponentGlobalVariable,
+  Statement,
+} from './ast';
 
 const projectFolder = path.join(__dirname, '../../ss-in-home/app/frontend');
 
 const angularFile = path.join(projectFolder, 'angular.json');
 
-const project = new Project({
-  tsConfigFilePath: path.join(projectFolder, 'tsconfig.json'),
-});
-
 let projectAST: Partial<AST> = {};
 
 if (fs.existsSync(angularFile)) {
+  const angularJson = JSON.parse(fs.readFileSync(angularFile, 'utf-8'));
+  const prefix: string = angularJson.projects[Object.keys(angularJson.projects)[0]].prefix;
+
+  const project = new Project({
+    tsConfigFilePath: path.join(projectFolder, 'tsconfig.json'),
+  });
+
   try {
     const mainPath = path.join(projectFolder, 'src/main.ts');
     const mainFile = project.getSourceFileOrThrow(mainPath);
@@ -22,6 +34,7 @@ if (fs.existsSync(angularFile)) {
     projectAST = {
       angular: {
         main: {
+          prefix,
           path: mainPath,
         },
       },
@@ -36,7 +49,7 @@ if (fs.existsSync(angularFile)) {
         const mainModulePath = getImportPathFromFile(mainFile, mainModule);
 
         if (mainModulePath) {
-          const mainModuleAbsolutePath = getPathFromFileImport(mainPath, mainModulePath);
+          const mainModuleAbsolutePath = getAbsolutePathFromFileImport(mainPath, mainModulePath);
           const mainModuleFile = project.getSourceFileOrThrow(mainModuleAbsolutePath);
 
           projectAST.angular!.main.module = {
@@ -49,24 +62,26 @@ if (fs.existsSync(angularFile)) {
             .getDecoratorOrThrow('NgModule')
             .getCallExpressionOrThrow()
             .getArguments()
-            .forEach((arg) => {
-              if (Node.isObjectLiteralExpression(arg)) {
-                arg.getProperties().forEach((prop) => {
-                  if (Node.isPropertyAssignment(prop)) {
-                    const initializer = prop.getInitializerOrThrow();
+            .forEach((argument) => {
+              if (Node.isObjectLiteralExpression(argument)) {
+                argument.getProperties().forEach((property) => {
+                  if (Node.isPropertyAssignment(property)) {
+                    const initializer = property.getInitializerOrThrow();
 
                     if (Node.isArrayLiteralExpression(initializer)) {
-                      if (prop.getName() === 'imports') {
-                        projectAST.angular!.main.moduleRouter = getRouterModuleFileFromInitializer(initializer, mainModuleFile);
+                      if (property.getName() === 'imports') {
+                        projectAST.angular!.main.moduleRouter = getRouterModuleFileFromInitializer(project, initializer, mainModuleFile);
                       }
 
-                      if (prop.getName() === 'bootstrap') {
+                      if (property.getName() === 'bootstrap') {
                         const bootstrapElement = initializer.getElements()[0].getText();
 
-                        projectAST.angular!.main.bootstrapElement = {
+                        const file = {
                           name: bootstrapElement,
-                          path: getPathFromFileImport(mainModuleAbsolutePath, getImportPathFromFile(mainModuleFile, bootstrapElement)!),
+                          path: getAbsolutePathFromFileImport(mainModuleAbsolutePath, getImportPathFromFile(mainModuleFile, bootstrapElement)!),
                         };
+
+                        projectAST.angular!.main.bootstrapElement = createComponentObject(project, file, prefix);
                       }
                     }
                   }
@@ -91,13 +106,265 @@ if (fs.existsSync(angularFile)) {
 
 console.log(JSON.stringify(projectAST, null, 2));
 
-function getRouterModuleFileFromInitializer(initializer: ArrayLiteralExpression, moduleFile: SourceFile): { path: string; name: string } | undefined {
+function removeStringQuotes(value: string | undefined): string {
+  return (value || '').replace(/\'/g, '').replace(/\"/g, '');
+}
+
+function createComponentObject(project: Project, file: ASTFile, prefix: string): Partial<Component> {
+  const component: Partial<Component> = {
+    file,
+  };
+
+  const source = project.getSourceFileOrThrow(file.path);
+  const componentClass = source.getClassOrThrow(file.name);
+  const argument = componentClass.getDecoratorOrThrow('Component').getCallExpressionOrThrow().getArguments()[0];
+
+  if (Node.isObjectLiteralExpression(argument)) {
+    argument.getProperties().forEach((property) => {
+      if (Node.isPropertyAssignment(property)) {
+        const name = property.getName();
+
+        if (name === 'selector' || name === 'templateUrl') {
+          let value: string | undefined;
+
+          if (Node.isStringLiteral(property.getInitializer())) {
+            value = property.getInitializer()?.getText();
+          }
+
+          if (value) {
+            if (name === 'selector') {
+              component.selector = removeStringQuotes(value);
+            }
+
+            if (name === 'templateUrl') {
+              component.templateUrl = getAbsolutePathFromFileImport(file.path, removeStringQuotes(value), false);
+            }
+          }
+        } else if (name === 'styleUrls') {
+          let value: string[] | undefined;
+
+          const initializer = property.getInitializer();
+
+          if (Node.isArrayLiteralExpression(initializer)) {
+            value = initializer
+              .getElements()
+              .map((element) => getAbsolutePathFromFileImport(file.path, removeStringQuotes(element.getText()), false));
+          }
+
+          if (value) {
+            component.styleUrls = value;
+          }
+        }
+      }
+    });
+  }
+
+  const globalVariables = componentClass.getMembers().reduce((previous, member) => {
+    if (Node.isPropertyDeclaration(member)) {
+      const type = member.getType();
+      const symbol = type.getSymbol();
+
+      previous[member.getName()] = {
+        modifier: member.getModifiers()?.[0]?.getText() || 'public',
+        usedInTemplate: false,
+        usedInComponent: false,
+        value: member.getInitializer()?.getText(),
+        type: symbol ? symbol.getName() : type.getText(),
+      };
+    }
+
+    return previous;
+  }, {} as Record<string, ComponentGlobalVariable>);
+
+  const constructor = componentClass.getConstructors()[0];
+
+  const parameters = constructor.getParameters().reduce((previous, parameter) => {
+    previous[parameter.getName()] = {
+      modifier: parameter.getModifiers()?.[0]?.getText() || 'public',
+      type: parameter.getType().getSymbol()?.getName()!,
+      isGlobal: !!parameter.getModifiers()?.[0]?.getText(),
+      isInternal: isLocalImport(getImportPathFromFile(source, parameter.getType().getSymbol()?.getName()!)!),
+    };
+
+    return previous;
+  }, {} as Record<string, ComponentConstructorParameter>);
+
+  const statements: Statement[] = getStatementsFromBody(source, constructor.getBody());
+
+  const onInitStatements: Statement[] = getStatementsFromBody(source, componentClass.getMethod('ngOnInit')?.getBody());
+  const onDestroyStatements: Statement[] = getStatementsFromBody(source, componentClass.getMethod('ngOnDestroy')?.getBody());
+
+  component.component = {
+    globalVariables,
+    constructor: {
+      parameters,
+      statements,
+    },
+    implements: componentClass
+      .getHeritageClauses()[0]
+      .getTypeNodes()
+      .map((heritage) => heritage.getText()),
+    lifecycle: {
+      onInit: {
+        implemented: false,
+        statements: onInitStatements,
+      },
+      onDestroy: {
+        implemented: false,
+        statements: onDestroyStatements,
+      },
+    },
+  };
+
+  if (component.templateUrl) {
+    const file = fs.readFileSync(component.templateUrl, 'utf-8');
+
+    const lines = file.split('\n').filter((line) => line.indexOf(`<${prefix}-`) >= 0);
+
+    const elements: ComponentElement[] = lines.map((line) => {
+      line = line.trim();
+
+      const properties = line.match(/\[?\w+\]?="\w+"/g)?.reduce((previous, current) => {
+        const name = current
+          .match(/\[?\w+]?=/g)![0]
+          .replace('[', '')
+          .replace(']', '')
+          .replace('=', '');
+
+        previous[name] = {
+          static: !current.match(/\[\w+\]=/g),
+        };
+
+        const value = removeStringQuotes(current.match(/"\w+"/g)?.[0]);
+
+        if (component.component!.globalVariables[value]) {
+          component.component!.globalVariables[value].usedInTemplate = true;
+
+          previous[name].variable = value;
+        } else {
+          previous[name].value = value;
+        }
+
+        return previous;
+      }, {} as Record<string, ComponentElementProperty>);
+
+      return {
+        type: line.substring(1, line.indexOf(' ')),
+        properties: properties || {},
+      };
+    });
+
+    component.html = {
+      hasRouterOutlet: file.indexOf('<router-outlet>') >= 0 && file.indexOf('</router-outlet>') >= 0,
+      elements,
+    };
+  }
+
+  return component;
+}
+
+function getStatementsFromBody(file: SourceFile, constructorBody: Node<ts.Node> | undefined): Statement[] {
+  let statements: Statement[] = [];
+
+  if (constructorBody && Node.isBlock(constructorBody)) {
+    constructorBody.getStatements().forEach((statement) => {
+      const newStatement = createStatements(file, statement);
+
+      if (newStatement) {
+        statements.push(newStatement);
+      }
+    });
+  }
+
+  return statements;
+}
+
+function createStatements(file: SourceFile, nodeStatement: Node<ts.Node>): Statement | undefined {
+  let statement: Statement | undefined;
+
+  if (Node.isVariableStatement(nodeStatement)) {
+    const initializer = nodeStatement.getDeclarationList().getDeclarations()[0].getInitializer();
+
+    if (Node.isCallExpression(initializer)) {
+      statement = {
+        variableDeclaration: {
+          name: nodeStatement.getDeclarationList().getDeclarations()[0].getName(),
+          value: initializer.getExpression().getText(),
+          import: {
+            path: getImportPathFromFile(file, initializer.getExpression().getText())!,
+          },
+        },
+      };
+    }
+  } else if (Node.isExpressionStatement(nodeStatement)) {
+    const expression = nodeStatement.getExpression();
+
+    if (Node.isCallExpression(expression)) {
+      statement = {
+        callExpression: {
+          calls: expression.getText().split('.'),
+          parameters: expression.getArguments().map((argument) => argument.getText()),
+        },
+      };
+    } else if (Node.isBinaryExpression(expression)) {
+      const left = expression.getLeft();
+      const right = expression.getRight();
+
+      if (Node.isCallExpression(right)) {
+        const rightExpression = right.getExpression();
+
+        if (rightExpression.getText().endsWith('.subscribe')) {
+          const arrowFunction = right.getArguments()[0];
+
+          if (Node.isArrowFunction(arrowFunction)) {
+            statement = {
+              binaryExpression: {
+                property: (Node.isPropertyAccessExpression(left) && left.getName()) || '',
+                subscribeExpression: {
+                  // variable: rightExpression.getText().split('.')[0],
+                  calls: rightExpression.getText().split('.'),
+                  parameters: arrowFunction.getParameters().map((parameter) => ({
+                    name: parameter.getName(),
+                    type: parameter.getType().getText(),
+                  })),
+                  statements: getStatementsFromBody(file, arrowFunction.getBody()),
+                },
+              },
+            };
+          }
+        }
+      } else if (Node.isIdentifier(right)) {
+        statement = {
+          binaryExpression: {
+            property: expression.getLeft().getText(),
+            value: right.getText(),
+          },
+        };
+      }
+    }
+  } else if (Node.isIfStatement(nodeStatement)) {
+    statement = {
+      if: {
+        condition: nodeStatement.getExpression().getText(),
+        statements: getStatementsFromBody(file, nodeStatement.getThenStatement()),
+      },
+    };
+  }
+
+  return statement;
+}
+
+function getRouterModuleFileFromInitializer(
+  project: Project,
+  initializer: ArrayLiteralExpression,
+  moduleFile: SourceFile
+): { path: string; name: string } | undefined {
   for (const element of initializer.getElements()) {
     if (Node.isIdentifier(element)) {
       const relativePath = getImportPathFromFile(moduleFile, element.getText());
 
       if (relativePath && isLocalImport(relativePath)) {
-        const importPath = getPathFromFileImport(moduleFile.getFilePath().toString(), relativePath);
+        const importPath = getAbsolutePathFromFileImport(moduleFile.getFilePath().toString(), relativePath);
 
         const isRouterModuleImport = project
           .getSourceFile(importPath)
@@ -141,11 +408,11 @@ function isLocalImport(importPath: string): boolean {
   return importPath.startsWith('.');
 }
 
-function getPathFromFileImport(filePath: string, importPath: string): string {
+function getAbsolutePathFromFileImport(filePath: string, importPath: string, isTypescript = true): string {
   const part = filePath.split('/');
   part.pop();
 
-  return path.join(part.join('/'), importPath + '.ts');
+  return path.join(part.join('/'), `${importPath}${isTypescript ? '.ts' : ''}`);
 }
 
 function getMainModuleName(statement: Node): string | undefined {
--- src/ast.ts
+++ src/ast.ts
@@ -3,13 +3,101 @@ export type ASTFile = {
   name: string;
 };
 
+export type ComponentElementProperty = {
+  static: boolean;
+  value?: any;
+  variable?: string;
+};
+
+export type ComponentElement = {
+  type: string;
+  properties: Record<string, ComponentElementProperty>;
+};
+
+export type ComponentGlobalVariable = {
+  type: string;
+  value: any;
+  modifier: string;
+  usedInTemplate: boolean;
+  usedInComponent: boolean;
+};
+
+export type ComponentConstructorParameter = {
+  modifier: string;
+  type: string;
+  isInternal: boolean;
+  isGlobal: boolean;
+};
+
+export type Statement = {
+  variableDeclaration?: {
+    name: string;
+    value: string;
+    import?: {
+      path: string;
+    };
+  };
+  callExpression?: {
+    from?: 'parameter' | 'global';
+    // variable: string;
+    calls: string[];
+    parameters: string[];
+  };
+  binaryExpression?: {
+    property: string;
+    value?: string;
+    subscribeExpression?: {
+      calls: string[];
+      parameters: Array<{
+        type: string;
+        name: string;
+      }>;
+      statements: Statement[];
+    };
+  };
+  if?: {
+    condition: string;
+    statements: Statement[];
+  };
+};
+
+export type Component = {
+  file: ASTFile;
+  selector: string;
+  templateUrl: string;
+  styleUrls: string[];
+  html: Partial<{
+    hasRouterOutlet: boolean;
+    elements: Array<ComponentElement>;
+  }>;
+  component: {
+    globalVariables: Record<string, ComponentGlobalVariable>;
+    constructor: {
+      parameters: Record<string, ComponentConstructorParameter>;
+      statements: Statement[];
+    };
+    implements: string[];
+    lifecycle?: {
+      onInit?: {
+        implemented: boolean;
+        statements: Statement[];
+      };
+      onDestroy?: {
+        implemented: boolean;
+        statements: Statement[];
+      };
+    };
+  };
+};
+
 export type AST = {
   angular: {
     main: Partial<{
+      prefix: string;
       path: string;
       module: ASTFile;
       moduleRouter: ASTFile;
-      bootstrapElement: ASTFile;
+      bootstrapElement: Partial<Component>;
     }>;
   };
 };
